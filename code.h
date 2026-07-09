bb379e1f8d22626c92b5339e940bd6172a81896b
rohayem <rohayemmahfouz123456@gmail.com>
2026-07-09 05:04:22 +0300

    Add files via upload

---
+++ code.h
@@ -0,0 +1,9 @@
+void scanArray(char array[],int size);
+void displayArraybefore(char array[],int size);
+
+void sortArrayr_Ascending_order(char array[],int size);
+void displayArrayafter_Ascending_order(char array[],int size);
+
+
+void sortArrayr_Descending_order(char array[],int size);
+void displayArrayafter_Descending_order(char array[],int size);
