a35ae7cdb6d301e970f518648c9f1645f3f697c2
rohayem <rohayemmahfouz123456@gmail.com>
2026-07-11 01:41:51 +0300

    Stdtype for following Types:

---
+++ Stdtype for following Types:/test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef unsigned char           U8_t;
+
+typedef signed char             S8_t;
+
+typedef unsigned short int      U16_t;
+
+typedef signed short int        S16_t;
+
+typedef unsigned long int       U32_t;
+
+typedef signed long int         S32_t;
+
+typedef unsigned long long int  U64_t;
+
+typedef signed long long int    S64_t;
+
+typedef float                   F32_t;
+
+typedef double                  F64_t;
+
+typedef long double             F128_t;
+
+int main()
+{
+    U8_t x = 255;
+    S16_t y = -100;
+    U32_t z = 50000;
+
+    printf("%u\n", x);
+    printf("%d\n", y);
+    printf("%lu\n", z);
+    printf("%d\n", rohayem);
+
+    return 0;
+}
