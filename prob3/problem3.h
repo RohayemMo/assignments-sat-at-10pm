f921e2388c697bc759e073d09a9b0577e8b116a2
rohayem <rohayemmahfouz123456@gmail.com>
2026-07-12 07:19:14 +0300

    Assignments(2)

---
+++ prob3/problem3.h
@@ -0,0 +1,9 @@
+#ifndef     PROB3
+#define     PROB3
+
+// typedef char     C8_t;
+typedef signed int   S32_t;
+S32_t sumMainDiagonal(int matrix[3][3]);
+void searchElement(S32_t *ptr, S32_t target);
+
+#endif
