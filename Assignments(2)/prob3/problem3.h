50904f854fcffee10c19305b3668753c18d8d863
rohayem <rohayemmahfouz123456@gmail.com>
2026-07-12 07:25:33 +0300

    Assignments(2)

--- Assignments(2)/prob3/problem3.h
+++ Assignments(2)/prob3/problem3.h
@@ -3,7 +3,7 @@
 
 // typedef char     C8_t;
 typedef signed int   S32_t;
-S32_t sumMainDiagonal(int matrix[3][3]);
+S32_t sumMainDiagonal(S32_t matrix[3][3]);
 void searchElement(S32_t *ptr, S32_t target);
 
 #endif
